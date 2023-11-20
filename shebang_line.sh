824a881f4c668f7082f3159ce56ea03b04e91ce8
Amlan Das <dasamlan271988@gmail.com>
2023-11-20 20:11:01 +0530

    From amlan

--- shebang_line.sh
+++ shebang_line.sh
@@ -3,7 +3,7 @@
 
 echo -e "\e[31;43m Enter your name:\e[0m\t"
 read name
-echo -e "\e[36m Enter your ID:\e[0m\t"
+echo -e "\e[36;41m Enter your ID:\e[0m\t"
 read id
 useradd -u $id $name
 echo -e "\e[94m User $name with id $id has been added succesfully\e[0m\n"
