8bbdd65c6a452688846009dc8b65f1c6de8825ac
Amlan Das <dasamlan271988@gmail.com>
2023-11-20 18:22:06 +0530

    From amlan

--- shebang_line.sh
+++ shebang_line.sh
@@ -1,6 +1,9 @@
 
 #!/bin/bash
 
-echo "Hello welcome to the first program"
-
-echo -e "\tHello!!\n\tHow are you"
+echo -e "Enter your name:\t"
+read name
+echo -e "enter your ID:\t"
+read id
+useradd -u $id $name
+echo -e "User $name has been added succesfully\n"
