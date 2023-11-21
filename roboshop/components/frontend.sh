a24aa8a7acfc805d0c3939cf5062c1d981101a48
Amlan Das <dasamlan271988@gmail.com>
2023-11-21 19:23:34 +0530

    From amlan

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,7 +6,7 @@ user_id=$(id -u)
 component=$1
 
 if [ $user_id -ne 0 ];then
-        echo -e"\e[32m Please run the program as sudo user \e[0m"
+        echo -e "\e[32m Please run the program as sudo user \e[0m"
 #        echo -e"\e[31m Example usage:\n \t\t \e[0m sudo bash scriptname componentname"
         exit 1
 fi
