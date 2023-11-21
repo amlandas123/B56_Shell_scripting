826646d4d421ad5b3194413eb41993e9470e67c8
Amlan Das <dasamlan271988@gmail.com>
2023-11-21 19:20:43 +0530

    From amlan

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -30,3 +30,14 @@ else
     echo -e "\e[31m Failure \e[0m"
 fi 
 
+
+echo -n "Enabling Nginx"
+systemctl enable nginx &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m Successfully Enabled \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+fi 
+
+
+
