006f1fc8122f3d2f811f62c8629e4644591a4d18
Amlan Das <dasamlan271988@gmail.com>
2023-11-21 08:29:05 +0530

    From amlan

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -2,7 +2,7 @@
 
 bash components/$1.sh
 
-if [ $? -nq 0 ]; then
+if [ $? -ne 0 ]; then
         echo -e "\e[31m You have entered wrong input \e[0m"
         echo -e "\e[31m For Example: \e[0m: bash wrapper.sh <components name>"
 fi        
