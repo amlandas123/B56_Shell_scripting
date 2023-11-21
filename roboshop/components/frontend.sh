28240a20bfecca50cffc244a8bbfd07aeee0e869
Amlan Das <dasamlan271988@gmail.com>
2023-11-21 16:30:55 +0530

    From amlan

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
 echo -e "\e[32m ******Configuring Frontend******\e[0m"
+
+user_id=$(id -u)
+component=$1
+
+if [ $user_id -ne 0 ];then
+        echo -e"\e[32m Please run the program as sudo user \e[0m"
+        echo -e"\e[31m Example usage:\n \t\t \e[0m sudo bash scriptname componentname"
+
+fi
+
+echo -e "\e[32m ******Configuring Frontend******\e[0m"
+
+echo -n "Installing Nginx:"
+yum install -y nginx &>> /tmp/frontend.log
+
+if [ $? -eq 0 ];then
+        echo -e "\e[32m Nginx Installed Successfully [0m"
+else
+        echo -e "\e[31m Nginx Installation failed [0m"
+fi
+
+echo -n "Downloading Component $1 :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+fi 
+
