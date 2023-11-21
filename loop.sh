a63f1c1bd0d3280e8fa0574387e8e409525b73e0
Amlan Das <dasamlan271988@gmail.com>
2023-11-21 12:02:23 +0530

    From amlan

--- loop.sh
+++ loop.sh
@@ -3,7 +3,7 @@
 n=7
 for (( i=1 ; i<=$n ; i++ )); 
 do
-    if [ ((i % 2)) -eq 0 ];then 
-        echo -e "\e[32m $num \e[0m"
+    if [ (i % 2) -eq 0 ];then 
+        echo -e "\e[32m $i \e[0m"
     fi    
 done
