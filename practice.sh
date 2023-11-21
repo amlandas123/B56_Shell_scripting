81e580e0ec95844c3e3439bc4388f00aea8d9c15
Amlan Das <dasamlan271988@gmail.com>
2023-11-21 08:12:49 +0530

    From amlan

--- practice.sh
+++ practice.sh
@@ -6,10 +6,10 @@ echo -e "\e[32m Enter Your age: \e[0m \t]"
 read age
 
 if [ $age -lt 18 ]; then
-      echo "\e[31m You are not eligible to Enter \e[0m"
+      echo -e "\e[31m You are not eligible to Enter \e[0m"
       exit 0
 else 
-      echo "\e[36m Welcome $name to the Hall \e[0m"
+      echo -e "\e[36m Welcome $name to the Hall \e[0m"
       exit 1
 fi
 
