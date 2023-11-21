29c38380f3f90f8b48e9074e39b093069c745c23
Amlan Das <dasamlan271988@gmail.com>
2023-11-21 08:03:32 +0530

    From amlan

--- practice.sh
+++ practice.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+echo -e "\e[32m Enter Your name: \e[0m \t"
+read name
+echo -e "\e[31m Enter Your age: \e[0m \t]"
+read age
 
-output.txt << ls -ltr
+if [ $age -lt 18 ]; then
+      echo " You are not eligible to Enter"
+else 
+      echo " Welcome $name to the Hall
+fi
+
+          
