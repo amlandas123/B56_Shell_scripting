0b3b88fe09ebe2003cb35af92cb9fa4461b903fd
Amlan Das <dasamlan271988@gmail.com>
2023-11-22 08:35:44 +0530

    From amlan

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,60 +3,57 @@
 #echo -e "\e[32m ******Configuring Frontend******\e[0m"
 
 user_id=$(id -u)
-component=$1
+component=frontend
+app_log= /tmp/${component}.log
 
 if [ $user_id -ne 0 ];then
         echo -e "\e[32m Please run the program as sudo user \e[0m"
         exit 1
 fi
+stat(){
+    if [ $1 -eq 0 ];then
+        echo -e "\e[32m Success [0m"
+    else
+        echo -e "\e[31m Failure [0m"
+    fi
+}
 
-
-echo -e "\e[32m ******Configuring Frontend******\e[0m"
+echo -e "\e[32m ******Configuring $component******\e[0m"
 
 echo -n "Installing Nginx:"
-yum install -y nginx &>> /tmp/frontend.log
+yum install -y nginx &>> app_log
+stat $?
 
-if [ $? -eq 0 ];then
-        echo -e "\e[32m Nginx Installed Successfully [0m"
-else
-        echo -e "\e[31m Nginx Installation failed [0m"
-fi
 
-echo -n "Downloading Component $1 :"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-fi 
-
+echo -n "Downloading $Component  :"
+curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/$component/archive/main.zip"
+stat $?
 
 echo -n "Enabling Nginx"
-systemctl enable nginx &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Successfully Enabled \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-fi 
+systemctl enable nginx &>> app_log
+stat $?
 
 echo -n "starting Nginx"
-systemctl start nginx &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Successfully Started \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-fi 
+systemctl start nginx &>> app_log
+stat $?
+
 
 echo -n " Nginx Status"
-systemctl status nginx &>> /tmp/frontend.log
+systemctl status nginx &>> app_log
 
+echo -n "Component cleanup"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
+stat $?
+
+echo -n "Component Extraction"
+unzip /tmp/${component}.zip >> app_log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[32m ******Configuring $component Successful******\e[0m"
 
 
 
