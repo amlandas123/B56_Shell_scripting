52af51dcfe4fe226e268ad128c56856a8c2f71ae
Amlan Das <dasamlan271988@gmail.com>
2023-11-24 08:22:22 +0530

    From amlan

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,55 +1,23 @@
 #!/bin/bash
 
 echo -e "\e[32m ******Configuring catalogue******\e[0m"
-source components/common.sh
 component=catalogue
-
 source components/common.sh
+NodeJS
+echo -e "***** \e[35m $component Configuration Is Completed \e[0m ******"
 
-#echo -n " NodeJS installation: "
-#curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y &>> $Logfile
-stat $?
 
-echo -n "Adding Application User: "
-id $App_user
-if [ $? -ne 0 ];then
-    useradd $App_user
-    stat $?
-else
-    echo -e "\e[31m User Exist \e[0m"
-fi
 
-echo -n "Download components: "
-curl -s -L -o /tmp/${component}.zip $cat_url
-stat $?
 
-echo -n "Clean Up filesexisting components: "
-rm -rf $Appuser_home &>> $Logfile
-stat $?
 
-echo -n "Extracting components: "
-cd /home/roboshop
-unzip /tmp/${component}.zip &>> $Logfile
-stat $?
 
-echo -n "Installing Components: "
-mv catalogue-main catalogue
-cd $Appuser_home
-npm install &>> $Logfile
-stat $?
 
-echo -n "Updating Mongodb DNS: "
-sed -i -e 's/MONGO_DNSNAME/mongod.roboshop.internal/' $Appuser_home/systemd.service
-stat $?
 
-echo -n "Updating Mongod service: "
-mv $Appuser_home/systemd.service /etc/systemd/system/${component}.service
-stat $?
 
-echo -n "Starting ${component} service: "
-systemctl daemon-reload
-systemctl enable ${component} &>> $Logfile
-systemctl start ${component}
-stat $?
+
+
+
+
+
+
 
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -18,3 +18,71 @@ stat() {
             echo -e "\e[31m Failure \e[0m"
     fi
 }
+
+create_user(){
+    echo -n "Adding Application User: "
+    id $App_user
+    if [ $? -ne 0 ];then
+        useradd $App_user
+        stat $?
+    else
+        echo -e "\e[31m User Exist \e[0m"
+    fi
+}
+
+download_cleanup_extract(){
+    echo -n "Download components: "
+    curl -s -L -o /tmp/${component}.zip $cat_url
+    stat $?
+
+    echo -n "Clean Up filesexisting components: "
+    rm -rf $Appuser_home &>> $Logfile
+    stat $?
+
+    echo -n "Extracting components: "
+    cd /home/roboshop
+    unzip /tmp/${component}.zip &>> $Logfile
+    stat $?
+}
+
+config_components(){
+    echo -n "Configuring $component permissions: "
+    mv ${Appuser_home}-main $Appuser_home
+    chown -R ${App_user}:${App_user} $Appuser_home
+    chmod 770 $Appuser_home
+    stat $?
+
+    echo -n "configuring $component systemd file: "
+    sed -i -e 's/MONGO_DNSNAME/mongod.roboshop.internal/' $Appuser_home/systemd.service
+    mv $Appuser_home/systemd.service /etc/systemd/system/${component}.service
+    stat $?
+
+}
+
+service_start(){
+    echo -n "Starting ${component} service: "
+    systemctl daemon-reload
+    systemctl enable ${component} &>> $Logfile
+    systemctl start ${component}
+    stat $?
+}
+
+NodeJS(){
+    echo -n " NodeJS installation: "
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+    yum install nodejs -y &>> $Logfile
+    stat $?
+
+    create_user     #call create user function
+    download_cleanup_extract  #call function to download,cleanup and extract
+    config_components   #call function for configuring components
+
+    echo -n "Generating Artifacts for application: "
+    cd $Appuser_home
+    npm install &>> $Logfile
+    stat $?
+
+    service_start       #call function to start service
+
+
+}
