6686423d181b4171e4a39c2e627c7c3605cfa6b2
Amlan Das <dasamlan271988@gmail.com>
2023-11-26 22:42:16 +0530

    From amlan

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 echo -e "\e[32m ******Configuring mysql******\e[0m"
+
+component=mysql
+
+source components/common.sh
+
+echo -n "***** Configuring $component *****"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+stat $?
+
+echo -n "***** Installing $component *****"
+yum install mysql-community-server -y
+stat $?
+
+echo -n "**** starting $component ****"
+systemctl enable $component &>> $Logfile
+systemctl start $component &>> $Logfile
+systemctl status $component &>> $Logfile
+stat $?
+
+
+
