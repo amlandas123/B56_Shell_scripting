68edcea4f8f877f80935d2992adf31231f0c9be7
Amlan Das <dasamlan271988@gmail.com>
2023-11-27 05:50:52 +0530

    From amlan

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -41,7 +41,7 @@ if [ $? -eq 0 ]; then
 fi 
 
 echo -n "Downloading $component Schema:"
-curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/${component}/archive/main.zip"
 stat $? 
 
 echo -n "Extracting Schema:"
