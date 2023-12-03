393c2e5bb48a24af844205c17ecf1770dfb7a717
Amlan Das <dasamlan271988@gmail.com>
2023-12-03 08:15:23 +0530

    From amlan

--- roboshop/stop.sh
+++ roboshop/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash(
 
-#This script is going to create EC2 instances
+#This script is going to terminate EC2 instances
 if [ -z $1 ] || [ -z $2 ]; then
         echo -e "\e[31m You have entered wrong input . Please Supply the argument\e[0m"
         echo -e "\e[31m For Example: \e[0m: bash create-ec2.sh <components name>"
@@ -8,19 +8,11 @@ if [ -z $1 ] || [ -z $2 ]; then
 fi 
 component=$1
 env=$2
-stopping=$3
-#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq .Images[].ImageId | sed -e 's/"//g')
-#SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=B56-Security-group |jq .SecurityGroups[].GroupId |sed -e 's/"//g')
-#Ins_type=t3.micro
-#Hosted_zone_id="Z07819082GXA8VTNL4M4B"
-
-
-
 
 stop_server(){
      echo -e "\e[36m $component-$env Server stopping In Progress \e[0m"
      instance_id=$(aws ec2 describe-instances  --filters "Name=tag-value,Values=${component}-${env}" | jq .Reservations[].Instances[].InstanceId | sed -e 's/"//g')
-     aws ec2 stop-instances --instance-ids ${instance_id}
+     aws ec2 terminate-instances --instance-ids ${instance_id}
      echo -e "\e[36m $component-$env Server is stopped \e[0m"
 
 }
