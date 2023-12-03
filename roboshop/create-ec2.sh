8a9f96f672dfe8a3082263860a3b20f3c4b2ea97
Amlan Das <dasamlan271988@gmail.com>
2023-12-03 06:17:24 +0530

    From amlan

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash(
 
 #This script is going to create EC2 instances
-if [ -z $1 ] || [-z $2 ]; then
+if [ -z $1 ] || [ -z $2 ]; then
         echo -e "\e[31m You have entered wrong input . Please Supply the argument\e[0m"
         echo -e "\e[31m For Example: \e[0m: bash create-ec2.sh <components name>"
         exit 0
@@ -23,7 +23,7 @@ create_server(){
     echo -e "\e[32m $component-${env} DNS record has been created \e[0m"
 
 }
-if [ $1 == "all"];then
+if [ $1 == "all" ];then
     for i in frontend mongodb catalogue user cart redis mysql shipping rabbitmq payment;do
         component=$i
         create_server
