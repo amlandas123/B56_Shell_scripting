aaff6a1cdcc5dbab67516375ebbcc25192ad123e
Amlan Das <dasamlan271988@gmail.com>
2024-02-18 21:39:09 +0530

    From amlan

---
+++ roboshop/components/create-user.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+stat(){
+    if [ $1 -eq 0 ];then
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[31m Failure \e[0m"
+    fi
+}
+
+create_user(){
+    echo -n "Adding Application User: "
+    id $username
+    if [ $? -ne 0 ];then
+        useradd $username
+        stat $?
+    else
+        echo -e "\e[31m User Already Exist \e[0m"
+    fi
+}
+
+echo -e "\e[36m Enter username to be added: \e[0m"
+read username
+create_user
