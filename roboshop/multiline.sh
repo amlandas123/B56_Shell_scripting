5151d28b4d4352f962314a66fbb274e14bddf870
Amlan Das <dasamlan271988@gmail.com>
2024-02-18 21:24:35 +0530

    From amlan

--- roboshop/multiline.sh
+++ roboshop/multiline.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-<<COMMENT
+
 echo -e  "\e[36m Welcome to shell scripting \e[0m"
 a=50
 b=100
 echo -e "\e[32m The valus of a is $a\e[0m"
-COMMENT
+
 
 echo -e "\e[32m Value of b is $b \e[0m"
 
