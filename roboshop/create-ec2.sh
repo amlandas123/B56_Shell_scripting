7cfadba38d2aa6ab9878a422e7ef6d4b19066cad
Amlan Das <dasamlan271988@gmail.com>
2024-04-04 20:34:51 +0530

    From amlan

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -9,7 +9,7 @@ fi
 component=$1
 env=$2
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq .Images[].ImageId | sed -e 's/"//g')
-SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=devops-priam |jq .SecurityGroups[].GroupId |sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Devops-normal-sg |jq .SecurityGroups[].GroupId |sed -e 's/"//g')
 Ins_type=t3.micro
 Hosted_zone_id="Z04859701G162VQQJD4H9"
 #instance_id=$(aws ec2 describe-instances  --filters "Name=tag-value,Values=user-dev" | jq .Reservations[].Instances[].InstanceId | sed -e 's/"//g')
